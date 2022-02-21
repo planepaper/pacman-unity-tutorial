865b88b71af30e610b388e644f178c4d2536a660
planepaper <eddysonkr@gmail.com>
2022-02-21 23:36:03 +0900

    FEAT: Implement GameController for playing
    - Make 'Ghost' and 'Pacman' class for Model.
    - Implement basic rules such as starting and overing game.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Ghost[] ghosts;
+    public Pacman pacman;
+    public Transform pellets;
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        foreach (Ghost ghost in ghosts)
+        {
+            ghost.gameObject.SetActive(true);
+        }
+
+        pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        foreach (Ghost ghost in ghosts)
+        {
+            ghost.gameObject.SetActive(false);
+        }
+
+        pacman.gameObject.SetActive(true);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhostEaten(Ghost ghost)
+    {
+        SetScore(score + ghost.Points);
+    }
+
+    public void PacmanEaten()
+    {
+        pacman.gameObject.SetActive(false);
+
+        SetLives(lives - 1);
+
+        if (lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    private readonly int points = 200;
+    public int Points
+    {
+        get => points;
+    }
+}
