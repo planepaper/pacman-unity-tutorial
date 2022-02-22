d45b414579fd59cd49ce15c90fb238fed5377ed2
planepaper <eddysonkr@gmail.com>
2022-02-22 22:16:03 +0900

    FEAT: Animate Pacman AnimatedSprite
    - Implement AnimatedSprite 'Class' for animating.

---
+++ Assets/Scripts/AnimatedSprite.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimatedSprite : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+    public float animationTime = 0.25f;
+    public int animationFrame { get; private set; }
+    public bool loop = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Advance), animationTime, animationTime);
+    }
+
+    private void Advance()
+    {
+        if (!spriteRenderer.enabled)
+        {
+            return;
+        }
+
+        animationFrame++;
+
+        if (animationFrame >= sprites.Length && loop)
+        {
+            animationFrame = 0;
+        }
+
+        if (animationFrame >= 0 && animationFrame < sprites.Length)
+        {
+            spriteRenderer.sprite = sprites[animationFrame];
+        }
+    }
+
+    public void Restrat()
+    {
+        animationFrame = -1;
+        Advance();
+    }
+}
