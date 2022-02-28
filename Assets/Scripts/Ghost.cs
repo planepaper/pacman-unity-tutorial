04f6e209c1f1a4af3c878e9eeb600bba7eb9100f
planepaper <eddysonkr@gmail.com>
2022-02-28 17:50:42 +0900

    FEAT: Implement GhostBehaviour Script
    - Move Ghost from their house.

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,10 +1,52 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Movement))]
 public class Ghost : MonoBehaviour
 {
+    public Movement movement { get; private set; }
+    public GhostBehavior initialBehavior;
+    public Transform target;
     private readonly int points = 200;
     public int Points
     {
         get => points;
     }
+
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    private void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        gameObject.SetActive(true);
+        movement.ResetState();
+
+        if (initialBehavior != null)
+        {
+            initialBehavior.Enable();
+        }
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        // Keep the z-position the same since it determines draw depth
+        position.z = transform.position.z;
+        transform.position = position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+
+        }
+    }
+
 }
